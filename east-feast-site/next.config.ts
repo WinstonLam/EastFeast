c799ec2d345b7e8e0a4f6f1eb80132e866fe55ff
WinstonLam <winston.lam@outlook.com>
2025-01-31 14:21:47 +0100

    updated config for pages

--- east-feast-site/next.config.ts
+++ east-feast-site/next.config.ts
@@ -1,8 +1,13 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  // Set the basePath and assetPrefix only in production
+  assetPrefix: isProd ? 'https://WinstonLam.github.io/EastFeast/east-feast-site' : '',
+  basePath: isProd ? '/east-feast-site' : '',
 };
 
-export default nextConfig;
+export default nextConfig
