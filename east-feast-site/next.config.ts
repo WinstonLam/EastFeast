549b47856616d696fd960f4e800a3f2263d9202d
WinstonLam <winston.lam@outlook.com>
2025-01-31 16:28:43 +0100

    updated hosting path

--- east-feast-site/next.config.ts
+++ east-feast-site/next.config.ts
@@ -6,8 +6,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   // Set the basePath and assetPrefix only in production
-  assetPrefix: isProd ? 'https://WinstonLam.github.io/EastFeast/east-feast-site' : '',
-  basePath: isProd ? '/east-feast-site' : '',
+  assetPrefix: isProd ? 'https://WinstonLam.github.io/EastFeast' : '',
+  basePath: isProd ? '/EastFeast' : '',
   output: "export"
 };
 
