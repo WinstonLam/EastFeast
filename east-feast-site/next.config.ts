b081c127a3da3994d88e782ed05fcf6f45b270e6
Winston Lam <winston.lam@outlook.com>
2025-02-01 12:09:44 +0100

    disable image optimization

--- east-feast-site/next.config.ts
+++ east-feast-site/next.config.ts
@@ -1,14 +1,17 @@
 // next.config.ts
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   // Set the basePath and assetPrefix only in production
-  assetPrefix: isProd ? 'https://WinstonLam.github.io/EastFeast' : '',
-  basePath: isProd ? '/EastFeast' : '',
-  output: "export"
+  assetPrefix: isProd ? "https://WinstonLam.github.io/EastFeast" : "",
+  basePath: isProd ? "/EastFeast" : "",
+  output: "export",
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
 };
 
-export default nextConfig
+export default nextConfig;
