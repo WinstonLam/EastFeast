72237d3eeeccc5bc2cdd0c05295261de4cf2803e
Winston Lam <winston.lam@outlook.com>
2025-02-05 10:22:45 +0100

    working on hero section

---
+++ east-feast-site/src/types/vanta.d.ts
@@ -0,0 +1,5 @@
+declare module "vanta/dist/vanta.fog.min" {
+  // You can replace "any" with a more specific type if you decide to type it later.
+  const VANTA_FOG: any;
+  export default VANTA_FOG;
+}
