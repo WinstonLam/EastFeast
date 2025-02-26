9f3680996d6308ff04276b7b815602b5fabf2122
WinstonLam <winston.lam@outlook.com>
2025-02-26 20:26:53 +0100

    added expansion card

--- east-feast-site/src/components/expandeddishcard.tsx
+++ east-feast-site/src/components/expandeddishcard.tsx
@@ -16,29 +16,42 @@ interface ExpandedDishCardProps {
 const ExpandedDishCard: React.FC<ExpandedDishCardProps> = ({ dish, onClose }) => {
     return (
         <div className="group relative bg-gradient-to-br from-prime to-second rounded-2xl shadow-2xl 
-        p-6 w-full h-full max-w-4xl mx-auto transition-all duration-300">
-
-            <button
-                onClick={onClose}
-                className="absolute top-4 left-4 z-50 p-2 bg-second rounded-full shadow-lg 
+        p-6 w-[90%] sm:w-full h-full max-w-[350px] sm:max-w-4xl flex flex-col justify-center sm:items-start items-center
+        mx-auto transition-all duration-300">
+            <div className="relative sm:absolute w-[150px] sm:w-[200px] md:w-[250px] 
+                rounded-xl drop-shadow-lg -top-16 sm:top-4 sm:right-4
+                scale-[1.8]  -translate-y-10 sm:translate-y-0
+             transition-all duration-300 ">
+                {dish.image}
+            </div>
+            <div className='hidden sm:block'>
+                <button
+                    onClick={onClose}
+                    className="absolute top-4 left-4 z-50 p-2 bg-second rounded-full shadow-lg 
                 hover:bg-prime text-white transition-all duration-300"
-            >
-                <IoClose className="w-8 h-8" />
-            </button>
+                >
+                    <IoClose className="w-8 h-8" />
+                </button>
+            </div>
 
-            <div className=" flex flex-row items-center w-[40%] md:w-[50%] ">
+            <div className=" flex flex-col sm:flex-row items-center -mt-14 sm:mt-10 md:mt-0 w-full md:w-[45%] sm:w-[40%]  ">
 
-                <div className="md:p-6 mt-12 md:mt-8 text-left text-background h-[275px] overflow-hidden text-ellipsis">
-                    <h3 className="text-3xl font-bold ">{dish.title}</h3>
-                    <hr className="bg-background border-0 h-[2px] rounded-xl w-[90%] mt-2" />
-                    <p className="mt-2 clamp(12px, 0.5vw, 20px)">{dish.desc}</p>
+                <div className="md:p-6 sm:mt-8 text-left text-background sm:h-[300px]  ">
+                    <div className='flex justify-between'>
+                        <h3 className="text-4xl sm:text-3xl font-bold sm:translate-y-0 ">{dish.title}</h3>
+
+                        <button
+                            onClick={onClose}
+                            className="sm:hidden top-4 left-4 z-50 p-2 bg-second rounded-full shadow-lg 
+                hover:bg-prime text-white transition-all duration-300"
+                        >
+                            <IoClose className="w-8 h-8" />
+                        </button></div>
+                    <hr className="bg-background border-0 h-[2px] rounded-xl w-[90%] mt-2 mb-2 sm:mt-6" />
+                    <p className=" sm:mt-0 clamp(12px, 0.5vw, 20px) h-[150px] overflow-scroll">{dish.desc}</p>
                     {/* You can add more detailed info here */}
                 </div>
-                <div className="absolute w-[200px] md:w-[250px] overflow-hidden 
-                rounded-xl drop-shadow-lg top-5 right-6 sm:right-5 scale-[1.7] 
-             transition-all duration-300">
-                    {dish.image}
-                </div>
+
             </div>
         </div>
     );
